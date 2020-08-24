35a45e9d505610cb4b239be5a1fda2abdb1fcbc5
Ben Perram <benperram@pop-os.localdomain>
2020-08-24 11:46:40 +1000

    Added layout to the home page following the spec
    Used an image form of the logo instead of the created one, so if I have time I will come back to change it.

--- part3/src/pages/home/home.js
+++ part3/src/pages/home/home.js
@@ -1,15 +1,22 @@
 import React from "react";
-
+import "./home.scss";
+import Globe from "./images/globe.png";
+import Logo from "./images/logo.png";
+import {FaRegHandPointRight} from 'react-icons/fa';
 
 export class Home extends React.Component {
     constructor(props) {
       super(props);
     }
-  
     render() {
       return (
         <div id="home-container">
-          YUh
+          <div id="get-started-container">
+            <h2 id="header-text">LOREM IPSUM</h2>
+            <img id="logo-image" alt="logo" src={Logo}></img>
+            <span id="get-started-text"><FaRegHandPointRight></FaRegHandPointRight> Click begin to sget started.</span>
+          </div>
+          <img id="globe-image" alt="globe" src={Globe}></img>
         </div>
       );
     }
